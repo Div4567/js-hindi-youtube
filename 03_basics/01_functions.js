cbd38b2087312b463043c6767980fbec34949f58
Div4567 <sinha.divya2005@gmail.com>
2025-08-10 19:33:47 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -43,7 +43,8 @@ function calculateCartPrice(val1, val2, ...num1){
     return num1
 }
 
-// console.log(calculateCartPrice(200, 400, 500, 2000))
+ console.log(calculateCartPrice(200, 400, 500, 2000)) 
+//output[500,2000]
 
 const user = {
     username: "hitesh",
